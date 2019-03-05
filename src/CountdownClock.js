c344cdfb9cf2931b669b104d91c5cabc29f5624b
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-03-05 16:31:46 -0500

    refactor: utils file

--- src/CountdownClock.js
+++ src/CountdownClock.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { convertSeconds, calculateTimeofCommit } from './utils';
 
 class CountdownClock extends Component {
   constructor() {
@@ -10,46 +10,25 @@ class CountdownClock extends Component {
     };
     this.timer = 0;
     this.tick = this.tick.bind(this);
+    this.addCommit = this.addCommit.bind(this);
   }
 
-  convertSeconds(secs) {
-    const days = Math.floor(secs / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((secs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((secs % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((secs % (1000 * 60)) / 1000);
-    return { days, hours, minutes, seconds };
+  async componentDidMount() {
+    const countdownDate = await calculateTimeofCommit();
+    this.setState({ countdownDate });
+    this.timer = setInterval(this.tick, 1000);
   }
 
-  async tick() {
+  tick() {
     const now = new Date().getTime();
     const distance = this.state.countdownDate - now;
     if (distance < 0) {
       clearInterval(this.timer);
     }
-    this.setState({ time: this.convertSeconds(distance) });
+    this.setState({ time: convertSeconds(distance) });
   }
 
-  async calculateTimeofCommit() {
-    const { data } = await axios.get(
-      'https://api.staging.coord.co/codechallenge/commits'
-    );
-    // multiply by 1000 to get milliseconds
-    const firstCommit = data[data.length - 1] * 1000;
-    const lastCommit = data[0] * 1000;
-
-    // extrapolate time of 2000th commit
-    const timeSoFar = lastCommit - firstCommit;
-    const commitsSoFar = data.length;
-    const commitsUntil2000 = 2000 - commitsSoFar;
-    const now = new Date().getTime();
-    return now + (commitsUntil2000 / commitsSoFar) * timeSoFar;
-  }
-
-  async componentDidMount() {
-    const countdownDate = await this.calculateTimeofCommit();
-    this.setState({ countdownDate });
-    this.timer = setInterval(this.tick, 1000);
-  }
+  addCommit() {}
 
   render() {
     return (
@@ -63,6 +42,7 @@ class CountdownClock extends Component {
         ) : (
           <p>Loading...</p>
         )}
+        <button onClick={this.addCommit}>Commit</button>
       </div>
     );
   }
---
+++ src/utils.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const convertSeconds = secs => {
+  const days = Math.floor(secs / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((secs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((secs % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((secs % (1000 * 60)) / 1000);
+  return { days, hours, minutes, seconds };
+};
+
+export const calculateTimeofCommit = async () => {
+  const { data } = await axios.get(
+    'https://api.staging.coord.co/codechallenge/commits'
+  );
+  // multiply by 1000 to get milliseconds
+  const firstCommit = data[data.length - 1] * 1000;
+  const lastCommit = data[0] * 1000;
+
+  // extrapolate time of 2000th commit
+  const timeSoFar = lastCommit - firstCommit;
+  const commitsSoFar = data.length;
+  const commitsUntil2000 = 2000 - commitsSoFar;
+  const now = new Date().getTime();
+  return now + (commitsUntil2000 / commitsSoFar) * timeSoFar;
+};
